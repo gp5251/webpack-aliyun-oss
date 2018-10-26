36864db9ed1a7f2edef8ff63ee1f0b1a4d022147
paul <34345790@qq.com>
2018-10-26 18:34:50 +0800

    bug fix; add func to delete empty directory; add func to custom upload path;

--- README.md
+++ README.md
@@ -3,6 +3,9 @@ A webpack(>=4) plugin to upload assets to aliyun oss
 
 一个webpack4插件，上传资源到阿里云cdn
 
+- 默认按output.path (webpack.config.js) 里的文件路径上传到oss，需要指定上传根目录(dist)。
+- 也可以通过`setOssPath`来配置不同的上传路径。
+
 Install
 ------------------------
 ```shell
@@ -12,13 +15,17 @@ $ npm i webpack-aliyun-oss -S
 Options
 ------------------------
 
-- `from`: 从哪里取文件上传，默认为output.path下所有的文件。支持类似gulp.src的glob方法，如'./build/**', 可以为glob字符串或者数组。
-- `dist`: 上传到哪个目录下，默认为根目录。
+- `from`: 上传哪些文件，默认为output.path下所有的文件。支持类似gulp.src的glob方法，如'./build/**', 可以为glob字符串或者数组。
+- `dist`: 上传到oss哪个目录下，默认为根目录。
 - `region`: 阿里云上传区域
 - `accessKeyId`: 阿里云的授权accessKeyId
 - `accessKeySecret`: 阿里云的授权accessKeySecret
 - `bucket`: 上传到哪个bucket
+- `timeout`: oss超时设置，默认为30秒(30000)
+- `verbose`: 是否显示上传日志，默认为true
 - `deletOrigin`: 上传完成是否删除原文件，默认false
+- `deleteEmptyDir`: 如果某个目录下的文件都上传到cdn了，是否删除此目录。deleteOrigin为true时候生效。默认false。
+- `setOssPath`: 自定义上传路径
 - `setHeaders`: 配置headers
 
 Example
@@ -35,8 +42,12 @@ const webpackConfig = {
     accessKeyId: 'your key',
     accessKeySecret: 'your secret',
     bucket: 'your bucket',
-    deleteOrigin: false,
+    setOssPath(filePath) {
+      // some operations to filePath
+      return '/new/path/flie.js';
+    },
     setHeaders(filePath) {
+      // some operations to filePath
       return {
         'Cache-Control': 'max-age=31536000'
       }
--- index.js
+++ index.js
@@ -1,48 +1,48 @@
-const fs        = require('fs');
-const path      = require('path');
-const oss       = require('ali-oss');
-const co        = require('co');
-const colors    = require('colors');
-const _         = require('lodash');
-const glob      = require("glob");
+const fs = require('fs');
+const path = require('path');
+const oss = require('ali-oss');
+const co = require('co');
+const colors = require('colors');
+const _ = require('lodash');
+const glob = require("glob");
 
 class WebpackAliyunOss {
     constructor(options) {
         this.config = Object.assign({
-            debug: false,
+            test: false,
+            verbose: true,
             dist: '',
             deleteOrigin: false,
-            deleteEmptyDir: true,
-            splitToken: '/build/',
+            deleteEmptyDir: false,
             timeout: 30 * 1000,
-            ossPathRules: [],
-            setHeaders() {
+            setOssPath: null,
+            setHeaders(filePath) {
                 return {}
             }
         }, options);
 
         this.configErrStr = this.checkOptions(options);
-        this.buildPath = '';
     }
 
     apply(compiler) {
-        compiler.hooks.afterEmit.tap('WebpackAliyunOss', (compilation) => {
+        compiler.hooks.afterEmit.tapPromise('WebpackAliyunOss', (compilation) => {
             if (this.configErrStr) {
                 compilation.errors.push(new Error(this.configErrStr));
                 return;
             }
 
-            this.buildPath = compiler.options.output.path;
-            const outputPath = this.buildPath;
-            const splitToken = this.config.splitToken || path.sep + outputPath.split(path.sep).pop() + path.sep;
+            const outputPath = compiler.options.output.path;
 
             const {
                 dist,
-                from = outputPath + (outputPath.endsWith(path.sep)? '' : path.sep) + '**',
+                from = outputPath + (outputPath.endsWith(path.sep) ? '' : path.sep) + '**',
                 setHeaders,
                 deleteOrigin,
                 deleteEmptyDir,
+                setOssPath,
                 timeout,
+                verbose,
+                test,
 
                 region,
                 accessKeyId,
@@ -59,51 +59,56 @@ class WebpackAliyunOss {
 
             const files = this.getFiles(from);
 
-            if (this.config.debug) {
-                console.log('files to be uploaded', files);
-                return;
-            }
-
-            const o = this;
             if (files.length)
-                co(function*() {
-                    let filePath, i = 0, len = files.length;
-                    while(i++ < len){
-                        filePath = files.shift();
-                        let filePathInBuildPath = filePath.split(splitToken)[1];
-                        let ossFilePath = dist + filePathInBuildPath;
-                        let result = yield client.put(ossFilePath, filePath, {
-                            timeout,
-                            headers: setHeaders(filePathInBuildPath)
-                        });
-                        console.log(filePath, 'upload to ' + ossFilePath + ' success'.green, 'cdn url =>', result.url.green);
+                return new Promise((resolve, reject) => {
+                    const o = this;
+                    const splitToken = path.sep + outputPath.split(path.sep).pop() + path.sep;
+                    co(function* () {
+                        let filePath, i = 0, len = files.length;
+                        while (i++ < len) {
+                            filePath = files.shift();
 
-                        if (deleteOrigin) {
-                            fs.unlink(filePath, ()=>{
+                            let ossFilePath = (dist + (setOssPath ? setOssPath(filePath) : (filePath.split(splitToken)[1] || ''))).replace(/\/\/+/g, '/');
+
+                            if (typeof ossFilePath !== 'string') continue;
+                            if (test) {
+                                console.log(filePath.gray, '\n is ready to upload to '.green + ossFilePath);
+                                continue;
+                            }
+
+                            let result = yield client.put(ossFilePath, filePath, {
+                                timeout,
+                                headers: setHeaders(filePath)
+                            });
+                            verbose && console.log(filePath.gray, '\nupload to '.green + ossFilePath + ' success,'.green, 'cdn url =>', result.url.green);
+
+                            if (deleteOrigin) {
+                                fs.unlinkSync(filePath);
                                 if (deleteEmptyDir && files.every(f => f.indexOf(path.dirname(filePath)) === -1))
                                     o.deleteEmptyDir(filePath);
-                            });
+                            }
                         }
-                    }
-                })
-                    .catch(err => {
-                        console.info('failed to upload to ali oss'.red, `${err.name}-${err.code}: ${err.message}`)
                     })
+                        .then(resolve, err => {
+                            console.info('failed to upload to ali oss'.red, `${err.name}-${err.code}: ${err.message}`)
+                            resolve()
+                        })
+                })
         });
     }
 
     getFiles(exp) {
-        const _getFiles = function(exp) {
+        const _getFiles = function (exp) {
             exp = exp[0] === '!' && exp.substr(1) || exp;
-            return glob.sync(exp, { nodir: true });
+            return glob.sync(exp, {nodir: true}).map(file => path.resolve(file))
         }
 
         return Array.isArray(exp) ?
-            exp.reduce((prev, next)=>{
+            exp.reduce((prev, next) => {
                 return next[0] === '!' ?
                     _.without(prev, ..._getFiles(next)) :
                     _.union(prev, _getFiles(next));
-            }, _getFiles(exp[0])):
+            }, _getFiles(exp[0])) :
             _getFiles(exp);
     }
 
@@ -115,7 +120,7 @@ class WebpackAliyunOss {
                 else {
                     if (!files.length) {
                         fs.rmdir(dirname)
-                        console.log('delete', dirname)
+                        this.config.verbose && console.log('empty directory deleted'.green, dirname)
                     }
                 }
             })
@@ -133,10 +138,10 @@ class WebpackAliyunOss {
 
         let errStr = '';
 
-        if (!region)            errStr += '\nregion not specified';
-        if (!accessKeyId)       errStr += '\naccessKeyId not specified'
-        if (!accessKeySecret)   errStr += '\naccessKeySecret not specified'
-        if (!bucket)            errStr += '\nbucket not specified'
+        if (!region) errStr += '\nregion not specified';
+        if (!accessKeyId) errStr += '\naccessKeyId not specified'
+        if (!accessKeySecret) errStr += '\naccessKeySecret not specified'
+        if (!bucket) errStr += '\nbucket not specified'
 
         let fromType = typeof from;
         if (['undefined', 'string'].indexOf(fromType) === -1 && !Array.isArray(from))
