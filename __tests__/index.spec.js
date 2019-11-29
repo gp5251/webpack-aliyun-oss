405ea86d8df3a31edc73ebbcae115e16cc1aa731
paul <34345790@qq.com>
2019-11-29 10:52:32 +0800

    update test

--- __tests__/index.spec.js
+++ __tests__/index.spec.js
@@ -1,6 +1,7 @@
 const WebpackAliyunOss = require('../');
 const fs = require('fs-extra')
 const path = require('path')
+const webpack = require('webpack');
 
 describe('webpack-aliyun-oss', () => {
 	const context = path.resolve(__dirname);
@@ -20,21 +21,39 @@ describe('webpack-aliyun-oss', () => {
 		expect(re).toBe('http://a.com/b/c')
 	});
 
-	it('should upload files', async () => {
+	it('can upload files widthout webpack', async () => {
 		const wpa = createWpaInstance({
 			from: ['./dist/**', '!./dist/*.html'], 
 			buildRoot: './dist'
 		});
 		const p = await wpa.doWidthoutWebpack();
-		console.log(p);
+		console.log('files', p);
 		expect(p.length).toBe(3);
+	});
+
+	it('can upload files in webpack', async () => {
+		const wpa = createWpaInstance({
+			from: ['./dist/**', '!./dist/*.(html|txt)']
+		});
+
+		const re = await runWebapck({
+			mode: 'production',
+			entry: './webpack/index.js',
+			output: {
+				path: path.resolve(__dirname, './dist')
+			},
+			plugins: [wpa]
+		})
+
+		expect(re).toBe('done');
 	});
 });
 
 function createWpaInstance(params = {}, test = true) {
+	const oss = require('./oss.config.js')
 	let config = {
 		from: './dist/**',
-		dist: '/temp/webpack-aliyun-oss',
+		dist: '/temp',
 		region: 'your region',
 		accessKeyId: 'your key',
 		accessKeySecret: 'your secret',
@@ -42,5 +61,25 @@ function createWpaInstance(params = {}, test = true) {
 		test
 	};
 
-	return new WebpackAliyunOss(Object.assign(config, params))
+	return new WebpackAliyunOss(Object.assign(config, oss, params))
+}
+
+function runWebapck(config) {
+	return new Promise((resolve, reject) => {
+		webpack(config, (err, stats) => {
+			if (err) {
+				console.error('err', err);
+				reject(err)
+			} else {
+				const info = stats.toJson();
+
+				if (stats.hasErrors()) {
+					console.error('error', JSON.stringify(info.errors, null, 2));
+					reject(info.errors)
+				}else {
+					resolve('done')
+				}
+			}
+		})
+	})
 }
